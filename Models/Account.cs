a14884b356885149c6ea3a352ed3336a67f40cdb
othukeodoko <executivedashboard@gtpensionmanagers.com>
2025-03-06 12:04:11 +0100

    Add normalized models with Identity and migrations

---
+++ Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingApp.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public decimal Balance { get; set; }
+        public int BankUserId { get; set; }
+        public BankUser BankUser { get; set; }
+    }
+}
